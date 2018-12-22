a719063c946d41aaf65d84ec6d58ab3fe1f1e955
Tyler Devers <devers.tyler@gmail.com>
2018-12-22 10:41:01 -0800

    fighting accessability error in create.cshtml.cs, must reslove to move on.

--- MicrosoftMVA/AspDotnetCoreBeginner/webApp1/WebApp1/Pages/Create.cshtml.cs
+++ MicrosoftMVA/AspDotnetCoreBeginner/webApp1/WebApp1/Pages/Create.cshtml.cs
@@ -9,8 +9,26 @@ namespace WebApp1.Pages
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        // Readonly so that whomever has access to this class cannot change the database
+        private readonly AppDbContext _db;
+        // Create constructor that asks for access to the database
+        public CreateModel(AppDbContext db)
         {
+            _db = db;
+        }
+        public Customer customer { get; set; }
+
+        // save information from the html form
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(customer);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("/");
         }
     }
 }
