d84e40bd31ab372358b29a7e70b961f2841a852d
Tyler Devers <devers.tyler@gmail.com>
2018-12-25 17:33:33 -0800

    completed list tutorials in dotnet

--- dotnetMSTutorials/list-tutorial/Program.cs
+++ dotnetMSTutorials/list-tutorial/Program.cs
@@ -8,6 +8,26 @@ namespace list_tutorial
         static void Main(string[] args)
         {
             //WorkingWithStrings();
+            //store the last two elements of the list in variables
+            FibonacciSequence(18);
+        }
+
+        private static void FibonacciSequence(int number)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            // increase the fibonacci sequence by the given number
+            for (int i = 0; i < number; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.Write(item + " ");
+            }
         }
 
         private static void WorkingWithStrings()
