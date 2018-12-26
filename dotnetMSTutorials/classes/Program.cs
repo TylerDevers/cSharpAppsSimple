f3b44c8e72580d89f6063177c31224ce3e55507b
Tyler Devers <devers.tyler@gmail.com>
2018-12-26 12:16:00 -0800

    completed 'introduction to classes' in dotnetMS

--- dotnetMSTutorials/classes/Program.cs
+++ dotnetMSTutorials/classes/Program.cs
@@ -7,7 +7,21 @@ namespace classes
         static void Main(string[] args)
         {
             var account = new BankAccount("Tyler", 1000);
-            Console.WriteLine($"Account {account.Number} created for {account.Owner} with a balance of {account.Balance}.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test for a negative balance
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
